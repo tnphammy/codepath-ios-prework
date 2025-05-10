51481b0742797cbf4a9bc9f584b9613e6a00fa86
tnphammy <tnpham@smith.edu>
2025-05-10 14:23:23 -0400

    added necessary files for submission

---
+++ Espoir/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Espoir
+//
+//  Created by tammy on 5/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor {
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
+            
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            
+        }
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    
+    
+}
+
---
+++ EspoirTests/EspoirTests.swift
@@ -0,0 +1,17 @@
+//
+//  EspoirTests.swift
+//  EspoirTests
+//
+//  Created by tammy on 5/10/25.
+//
+
+import Testing
+@testable import Espoir
+
+struct EspoirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
